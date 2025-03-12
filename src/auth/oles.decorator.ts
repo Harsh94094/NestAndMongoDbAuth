38e4198c433e0d67056e451a201d5a8c2744792a
Harsh94094 <harshgadhiya5949@gmail.com>
2025-03-12 10:13:43 +0530

    Revert "frist commit"
    This reverts commit 0b37f60e8d8c9f1f41a547abd0b1f2d2b715d4b9.

--- src/auth/oles.decorator.ts
+++
@@ -1,5 +0,0 @@
-// filepath: /home/xrstudio/Desktop/HarshFristProject/nestAndMongoauth/nest-auth/src/auth/roles.decorator.ts
-import { SetMetadata } from '@nestjs/common';
-import { Role } from '../users/users.entity';
-
-export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
